f0c7fc32fdacdc6f2a7ed5cbd7c363830115d859
Pushendra2020 <161145799+Pushendra2020@users.noreply.github.com>
2024-09-24 20:49:54 +0530

    Update register.js

--- public/register.js
+++ public/register.js
@@ -1,22 +1,24 @@
 document.getElementById('registerForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 
-    const name = document.getElementById('name').value;
+    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    
+    
     try {
-        const response = await fetch('http://localhost:5500/register', {
+        const response = await fetch('http://localhost:5501/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name, email, password })
+            body: JSON.stringify({ username, email, password })
         });
 
         const data = await response.text();
 
         if (response.ok) {
             alert('Account created successfully!');
-            window.location.href = 'login.html'; // Redirect to login page
+            window.location.href = 'login.html'; 
         } else {
             alert('Error: ' + data);
         }
